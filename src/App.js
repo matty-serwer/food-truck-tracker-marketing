ab5be88f826ac4946ca8dc4a9da3b5eacccab5a6
Matthew Serwer <Matthew.Serwer@gmail.com>
2020-10-23 14:32:34 -0400

    sorting errors

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Home from './components/pages/Home'
 import About from './components/pages/About'
 import Products from './components/pages/Products'
 import SignUp from './components/pages/SignUp'
-import About from './components/pages/About'
 
 function App() {
   return (
